b6c750ed060df13fa9896a97a7332db0399eebb5
Nitesh Chowdhary <thenitesh0112@gmail.com>
2023-03-27 04:48:07 +0530

    Add book to array

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const bookname = document.getElementById('book-name')
+const bookname = document.getElementById('bookname')
 const author = document.getElementById('author')
 const pages = document.getElementById('pages')
 const isRead = document.getElementById('is-Read')
 const bookarray = document.getElementById('book-array')
 const addBook = document.getElementById('add-book')
 
-addBook.addEventListener('click', addBookToArray)
+addBook.addEventListener('click', () => {
+    console.log(bookname.value, author.value, pages.value, isRead.value)
+    const book2 = new Book(bookname.value, author.value, pages.value, isRead.value)
+    console.log(book2.info())
+    myLibrary.push(book2)
+    console.log(myLibrary)
+})
 
 
 
 let myLibrary = []
+console.log(myLibrary)
 
 function Book(title, author, pages, isRead) {
     this.title = title
